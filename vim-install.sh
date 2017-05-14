85f1f0d8b9816f5dec8a1d518de2168715e16cc9
liangguohuan <liangguohuan@gmail.com>
2017-05-15 02:26:57 +0800

    update vim-install file

--- vim-install.sh
+++ vim-install.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
-git --version &>/dev/null
+# install depends
+sudo apt install curl git-core cmake python-dev
 
-if [[ $? == 0 ]]; then
-    mkdir -p ~/.vim/temp_dirs/undodir/
-    git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-    curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-    cp -f .vimrc ~/
-    cp -f .vim-basic.vim ~/
-    cp -f .vim-plugin.vim ~/
-    vim +PlugInstall +qall
-fi
+# pyenv install and virtualenv
+curl -L https://raw.githubusercontent.com/pyenv/pyenv-installer/master/bin/pyenv-installer | bash
+pyenv install 2.7.12
+pyenv virtualenv 2.7.12 neovim2
+pyenv install 3.6.1
+pyenv virtualenv 3.6.1 neovim3
+
+# install vim-plug
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+mkdir -p ~/.vim/temp_dirs/undodir/
+cp -f .vimrc ~/
+cp -f .vim-basic.vim ~/
+cp -f .vim-plugin.vim ~/
+vim +PlugInstall +qall
